57c82ec13e310578632b8bec847e6486b8c95120
morrison-page <104316350+morrison-page@users.noreply.github.com>
2024-05-08 19:21:01 +0100

    ...

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,22 @@
 oh-my-posh init pwsh --config 'C:\Users\morri\Documents\WindowsPowerShell\morrion-posh.json' | Invoke-Expression
+
+function ff($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        Write-Output "$($_.directory)\$($_)"
+    }
+}
+
+function pubip { (Invoke-WebRequest http://ifconfig.me/ip).Content }
+
+function uptime {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+    } else {
+        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+    }
+}
+
+function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
+
+function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
+
